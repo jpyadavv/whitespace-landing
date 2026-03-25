d9a1aaceb6d0c7a329d188e4f1a6ca1420ac047b
Jai Parkash <yadavjp808336@gmail.com>
2026-03-25 14:34:09 +0530

    Updated Testimonial and CtaBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar"
 import Extension from "./Extension"
-import ProductList from "./ProductList"
 import CtaBanner from "./CtaBanner"
 import Testimonial from "./Testimonial"
 import Footer from "./Footer"
